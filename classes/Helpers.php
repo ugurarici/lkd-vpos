238ef2a1f7469d595635d67ead936e4f1b93e05f
Uğur Arıcı <m.ugurarici@gmail.com>
2017-01-07 01:39:48 +0200

    Move createCaptcha to Helpers class

--- classes/Helpers.php
+++ classes/Helpers.php
@@ -5,6 +5,14 @@ class Helpers{
 		return substr($number, 0, 4) . str_repeat($maskingCharacter, strlen($number) - 8) . substr($number, -4);
 	}
 
+	static function createCaptcha(){
+		$captchaBuilder = new Gregwar\Captcha\CaptchaBuilder;
+		$captchaBuilder->buildAgainstOCR($width = 240, $height = 60, $font = null);
+		session_start();
+		$_SESSION['captchaPhrase'] = $captchaBuilder->getPhrase();
+		return $captchaBuilder->inline();
+	}
+
 	static function checkCaptcha($userAnswer){
 		//	bulmaca çözümü SESSION'da saklandığı için session'ı başlatıyoruz
 		session_start();
@@ -15,4 +23,5 @@ class Helpers{
 		if( $userAnswer !== $captchaAnswer )
 			throw new Exception("Bulmaca yanıtı yanlış", 1);
 	}
+
 }
--- createCaptcha.php
+++ createCaptcha.php
@@ -2,8 +2,4 @@
 // Formda gösterebilmek için captcha oluşturağız ve sonraki ekranda kontrol edebilmek adına içindeki veriyi de SESSION'a yerleştireceğiz
 //	@Author: Uğur ARICI [m.ugurarici@gmail.com]
 require "vendor/autoload.php";
-$captchaBuilder = new Gregwar\Captcha\CaptchaBuilder;
-$captchaBuilder->buildAgainstOCR($width = 240, $height = 60, $font = null);
-session_start();
-$_SESSION['captchaPhrase'] = $captchaBuilder->getPhrase();
-echo $captchaBuilder->inline();
+echo Helpers::createCaptcha();
